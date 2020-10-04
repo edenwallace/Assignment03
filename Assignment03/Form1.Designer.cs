63b7f4f502f42ab2bf3ba33fe25d2dc41bef4827
Eden Wallace <wallac33@nsuok.edu>
2020-10-03 21:49:38 -0500

    working game without speeds

--- Assignment03/Form1.Designer.cs
+++ Assignment03/Form1.Designer.cs
@@ -28,21 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameSpeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.slowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fastToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Bomb = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Bomb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameSpeedToolStripMenuItem,
+            this.instructionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameSpeedToolStripMenuItem
+            // 
+            this.gameSpeedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.slowToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.fastToolStripMenuItem});
+            this.gameSpeedToolStripMenuItem.Name = "gameSpeedToolStripMenuItem";
+            this.gameSpeedToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.gameSpeedToolStripMenuItem.Text = "Game Speed";
+            // 
+            // slowToolStripMenuItem
+            // 
+            this.slowToolStripMenuItem.Name = "slowToolStripMenuItem";
+            this.slowToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.slowToolStripMenuItem.Text = "Slow";
+            this.slowToolStripMenuItem.Click += new System.EventHandler(this.slowToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            // 
+            // fastToolStripMenuItem
+            // 
+            this.fastToolStripMenuItem.Name = "fastToolStripMenuItem";
+            this.fastToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.fastToolStripMenuItem.Text = "Fast";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.instructionsToolStripMenuItem.Text = "Instructions";
+            this.instructionsToolStripMenuItem.Click += new System.EventHandler(this.instructionsToolStripMenuItem_Click);
+            // 
+            // Bomb
+            // 
+            this.Bomb.BackColor = System.Drawing.Color.Transparent;
+            this.Bomb.Image = global::Assignment03.Properties.Resources.bomb;
+            this.Bomb.Location = new System.Drawing.Point(26, 72);
+            this.Bomb.Name = "Bomb";
+            this.Bomb.Size = new System.Drawing.Size(100, 98);
+            this.Bomb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Bomb.TabIndex = 1;
+            this.Bomb.TabStop = false;
+            this.Bomb.Tag = "Bomb";
+            this.Bomb.Click += new System.EventHandler(this.popBomb);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Assignment03.Properties.Resources.yellowBalloon;
+            this.pictureBox2.Location = new System.Drawing.Point(237, 333);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "Balloon";
+            this.pictureBox2.Click += new System.EventHandler(this.popBalloon);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Assignment03.Properties.Resources.redHeart;
+            this.pictureBox3.Location = new System.Drawing.Point(26, 246);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Balloon";
+            this.pictureBox3.Click += new System.EventHandler(this.popBalloon);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::Assignment03.Properties.Resources.pinkBalloon;
+            this.pictureBox4.Location = new System.Drawing.Point(168, 72);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 143);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "Balloon";
+            this.pictureBox4.Click += new System.EventHandler(this.popBalloon);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::Assignment03.Properties.Resources.redStar;
+            this.pictureBox5.Location = new System.Drawing.Point(26, 397);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "Balloon";
+            this.pictureBox5.Click += new System.EventHandler(this.popBalloon);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 550);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameEngine);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(484, 961);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Bomb);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Bomb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameSpeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem slowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fastToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox Bomb;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- Assignment03/Form1.cs
+++ Assignment03/Form1.cs
@@ -12,9 +12,146 @@ namespace Assignment03
 {
     public partial class Form1 : Form
     {
+        int speed = 5;
+        int score = 0;
+        Random rand = new Random();
+        bool gameOver = false; 
+
         public Form1()
         {
+            Instructions instructionsForm = new Instructions();
+            instructionsForm.Show();
+
             InitializeComponent();
+          
+            resetGame();
+
+            gameTimer.Stop(); 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void instructionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Instructions instructionsForm = new Instructions();
+            instructionsForm.Show();
+        }
+
+        private void gameEngine(object sender, EventArgs e)
+        {
+            label1.Text = " Score: " + score;
+
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox)
+                {
+                    X.Top -= speed;
+
+                    if (X.Top + X.Height < 0)
+                    {
+                        X.Top = rand.Next(400, 800);
+                        X.Left = rand.Next(5, 400); 
+                    }
+
+                    if (X.Tag.ToString() == "Balloon" && X.Top < -50)
+                    {
+                        gameTimer.Stop();
+                        label1.Text += "Game Over! - Press Enter to Retry";
+                        gameOver = true;
+                    }
+                }
+            }
+
+            if (score >= 10)
+            {
+                speed = 2;
+            }
+
+            if (score >= 20)
+            {
+                speed = 3;
+            }
+
+            if (score >= 35)
+            {
+                speed = 4;
+            }
+        }
+
+        
+
+        private void popBalloon(object sender, EventArgs e)
+        {
+            if (gameOver == false)
+            {
+                var balloon = (PictureBox)sender;
+                balloon.Top = rand.Next(400,800);
+                balloon.Left = rand.Next(5, 400);
+                score++;
+            }
+        }
+
+        private void popBomb(object sender, EventArgs e)
+        {
+            if (gameOver == false)
+            {
+                Bomb.Image = Properties.Resources.boom;
+                gameTimer.Stop();
+                label1.Text += " Game Over! - Press Enter to retry ";
+                gameOver = true; 
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                resetGame();
+
+                
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+
+
+                gameTimer.Start();
+
+
+            }
+        }
+
+        private void resetGame() 
+        {
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox)
+                {
+                    X.Top = rand.Next(400, 800);
+                    X.Left = rand.Next(5, 400);
+                }
+            }
+
+            Bomb.Image = Properties.Resources.bomb;
+            speed = 1;
+            score = 0;
+            gameOver = false;
+            label1.Text = "Score: " + score;
+            gameTimer.Start();
+        }
+
+        private void startgame(object sender, KeyPressEventArgs e)
+        {
+            
+            
+        }
+
+        private void slowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
